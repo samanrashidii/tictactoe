c1cf99bb279addcc063a463cf3cb232ed5e52d8f
Samanrashidii <samanrashidi44@gmail.com>
2024-05-12 03:04:16 -0700

    feature: implemented base logic for board squares

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,14 +2,27 @@ import Square from './Square';
 import { useState } from 'react';
 
 export default function App () {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null))
+
+    const handleClick = function (i) {
+        const newSquares = squares.slice();
+        if (xIsNext) {
+            newSquares[i] = 'X';
+        } else {
+            newSquares[i] = 'O';
+        }
+        setSquares(newSquares);
+        setXIsNext(!xIsNext);
+    }
 
     return (
       <div className="game-board">
-        {squares.map((square) => (
+        {squares.map((square, index) => (
           <Square
-            key={square}
+            key={index}
             value={square}
+            onSquareClick={() => handleClick(index)}
           />
         ))}
       </div>
